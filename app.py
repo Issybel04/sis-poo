f167604347e142f6006a5b18c4e34357d7d4bc5d
Isabel López <isabellacero17@gmail.com>
2023-07-24 21:08:22 -0600

    segundos cambios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,22 @@ students = []
 @app.route("/")
 def index():
     return render_template('index.html', students=students)
+
+@app.route("/new_student", methods=['GET', 'POST'])
+def new_student():
+    if request.method == "GET":
+        print("Método GET")
+        return render_template('new_student.html')
+    elif request.method == "POST":
+        estudiante = {
+            "nombre": request.form["nombre"],
+            "apellido_paterno": request.form["apellido_paterno"],
+            "apellido_materno": request.form["apellido_materno"],
+            "fecha_nacimiento": request.form["fecha_nacimiento"],
+            "matricula": request.form["matricula"]
+        }
+        students.append(estudiante)
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
