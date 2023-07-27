dd462fbfdc80bee66586e9b13d2a858edc919c1a
Isabel López <isabellacero17@gmail.com>
2023-07-26 19:39:28 -0600

    ya hice las tablas gg

---
+++ sis.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, matricula ):
+        self.nombre = nombre
+        self.apellido_pat = apellido_paterno
+        self.apellido_mat = apellido_materno
+        self.fecha = fecha_nacimiento
+        self.matricula = matricula
