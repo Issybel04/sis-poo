526e753f13d8ac2c475a4a87253fa60756425148
Isabel López <isabellacero17@gmail.com>
2023-08-01 13:20:22 -0600

    tarea de gera

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from entities.student import Student
 from entities.professor import Professor
+from entities.course import Course
 
 app = Flask(__name__)
 
 students_list = []
 professors_list = []
+courses_list = []
 
 
 @app.route("/")
@@ -21,6 +23,12 @@ def students():
 @app.route("/professors")
 def professors():
     return render_template('professors.html', professors_list=professors_list)
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html', courses_list=courses_list)
+
+
 
 
 @app.route("/new_student", methods=['GET', 'POST'])
@@ -55,6 +63,17 @@ def new_professor():
         professors_list.append(professor)
         return redirect('/professors')
 
+@app.route("/new_course", methods=['GET', 'POST'])
+def new_course():
+    if request.method == "GET":
+        print("Método GET")
+        return render_template('new_course.html')
+    elif request.method == "POST":
+        course = Course(
+            request.form["nombre"],
+        )
+        courses_list.append(course)
+        return redirect('/courses')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
