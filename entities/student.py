b04efa1dbe6c60fc5e7fa4e3267a8c88f53ba87f
Isabel López <isabellacero17@gmail.com>
2023-08-02 21:09:06 -0600

    Clase Persona

--- entities/student.py
+++ entities/student.py
@@ -1,7 +1,7 @@
-class Student:
-    def __init__(self, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, matricula ):
-        self.nombre = nombre
-        self.apellido_pat = apellido_paterno
-        self.apellido_mat = apellido_materno
-        self.fecha = fecha_nacimiento
-        self.matricula = matricula
+from entities.person import Person
+
+class Student(Person):
+    def __init__(self, nombre, ap_paterno, ap_materno, fech_nacimiento, matr):
+        super().__init__(nombre, ap_paterno, ap_materno, fech_nacimiento)
+        self.matricula = matr
+    
